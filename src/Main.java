5d5d968bc862e38aa9e9c05493aca46548835bce
Emil <isip_e.t.hodjakhov@mpt.ru>
2024-04-20 22:19:03 +0300

    Limon

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,91 @@
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) {
-        List<String> ProductList = ListProduct();
-        for (String Product : ProductList)
-        {
-            System.out.println(Product);
+        ArrayList<Worker> workersList = new ArrayList<>();
+        workersList.add(new Worker("Иван", 30, 50000));
+        workersList.add(new Worker("Петр", 25, 45000));
+        workersList.add(new Worker("Мария", 35, 55000));
+
+        addWorker(workersList);
+        editWorker(workersList);
+
+        ArrayList<Integer> idList = new ArrayList<>();
+        idList.add(1);
+        idList.add(2);
+        removeWorker(workersList, idList);
+
+        double avgSalary = getAverageSalary(workersList);
+        System.out.println("Средняя зарплата: " + avgSalary);
+
+        String workerToFind = "Петр";
+        int foundIndex = findWorker(workersList, workerToFind);
+        if (foundIndex != -1) {
+            System.out.println("Сотрудник " + workerToFind + " найден под индексом " + foundIndex);
+        } else {
+            System.out.println("Сотрудник " + workerToFind + " не найден");
         }
-        List<String> DolznotbList = SpisokDolznostey();
-        for (String Dolznostb : DolznotbList)
-        {
-            System.out.println(Dolznostb);
-        }
-        List<String> KolichestvoStankov = StankiQ();
-        for (String Stanki : KolichestvoStankov)
-        {
-            System.out.println(Stanki);
-        }
-        List<String> Material = Materials();
-        for (String Materials : Material)
-        {
-            System.out.println(Materials);
-        }
-        List <String> Services = Uslugi();
-        for (String Uslugi : Services)
-        {
-            System.out.println(Uslugi);
-        }
-    }
-    public static List<String> ListProduct()
-    {
-        List<String> ProductList = new ArrayList<>();
-        ProductList.add("Продукция:");
-        ProductList.add("Диван");
-        ProductList.add("Кровать");
-        ProductList.add("Гумибер");
-        ProductList.add("Максим");
-        ProductList.add("Табуретка");
-        return ProductList;
-    }
-    public static List<String> SpisokDolznostey()
-    {
-        List<String> DolznostbList = new ArrayList<>();
-        DolznostbList.add("Список должностей:");
-        DolznostbList.add("Аудитор");
-        DolznostbList.add("Аналитик");
-        DolznostbList.add("Администратор");
-        DolznostbList.add("Бухгалтер");
-        DolznostbList.add("Диспетчер");
-        return DolznostbList;
-    }
-    public static List<String> StankiQ()
-    {
-        List<String> KolichestvoStankov = new ArrayList<>();
-        KolichestvoStankov.add("Количество рабочих станков:");
-        KolichestvoStankov.add("15");
-        KolichestvoStankov.add("105");
-        KolichestvoStankov.add("666");
-        KolichestvoStankov.add("23");
-        KolichestvoStankov.add("32");
-        return KolichestvoStankov;
     }
 
-    public static List <String> Materials()
-    {
-        List <String> Material = new ArrayList<>();
-        Material.add("Материалы: ");
-        Material.add("Цельный массив дерева");
-        Material.add("Клееный массив дерева");
-        Material.add("Фанера");
-        Material.add("Древесно-стружечная плита");
-        return Material;
+    static void addWorker(ArrayList<Worker> workersList) {
+        for (Worker worker : workersList) {
+            System.out.println("Добавлен сотрудник " + worker.getName() + " с возрастом " + worker.getAge() + " с зарлатой " + worker.getSalary());
+        }
     }
 
-    public static List <String> Uslugi()
-    {
-        List <String> Services = new ArrayList<>();
-        Services.add("Услуги: ");
-        Services.add("Распил");
-        Services.add("Кромление");
-        Services.add("Покраска");
-        Services.add("Фрезеровка");
-        Services.add("Присадка");
-        return Services;
+    static void editWorker(ArrayList<Worker> workersList) {
+        for (Worker worker : workersList) {
+            System.out.println("Изменён сотрудник " + worker.getName() + ". Возраст: " + worker.getAge() + ". Зарплата: " + worker.getSalary());
+        }
+    }
+
+    static void removeWorker(ArrayList<Worker> workersList, ArrayList<Integer> idList) {
+        for (int id : idList) {
+            if (id >= 0 && id < workersList.size()) {
+                Worker removedWorker = workersList.remove(id);
+                System.out.println("Удалён сотрудник " + removedWorker.getName());
+            }
+        }
+    }
+
+    static double getAverageSalary(ArrayList<Worker> workersList) {
+        double totalSalary = 0;
+        for (Worker worker : workersList) {
+            totalSalary += worker.getSalary();
+        }
+        return totalSalary / workersList.size();
+    }
+
+    static int findWorker(ArrayList<Worker> workersList, String name) {
+        for (int i = 0; i < workersList.size(); i++) {
+            if (workersList.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class Worker {
+    private String name;
+    private int age;
+    private int salary;
+
+    public Worker(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 }
